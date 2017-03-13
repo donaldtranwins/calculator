ec5aef88d1508a502b9cdbc626d429f6057d312a
donaldtranwins <donaldtranwins@users.noreply.github.com>
2017-03-13 09:58:59 -0700

    Calculator v1.0 - Donald Tran (#3)
    Calculator v1.0 - Donald Tran (#3)

--- calculator.js
+++ calculator.js
@@ -1,33 +1,29 @@
-$(document).ready(function () {
-    applyHandlers();
-});
 
+/** *********
+ ** ** Global Variables
+ ** ********* **/
 var currentInput = [];
 var savedInputs = [];
 var lastButton = null;
 var numbersToCalculate = null;
-var positive = true;
 
+/** *********
+ ** ** Handlers
+ ** ********* **/
+$(document).ready(function () {
+    applyHandlers();
+});
 function applyHandlers(){
-    $('#calculator').on('click','.button div',buttonClicked);
-    $(document).on('keypress',validateKeypress);
     $(document).on('keydown',validateKeydown);
+    $(document).on('keypress',validateKeypress);
+    $('#calculator').on('click','.button div',buttonClicked);
 }
 function validateKeydown() {
-    if (event.which === 8) //backspace
+    if (event.which === 8) // Allows backspace key to delete one input
         buttonClicked("←");
-    if (event.which === 27) //escape
+    if (event.which === 27) // Allows escape key to clear all
         buttonClicked("AC");
 }
-// var keys = {
-//     '=': '=', '+': ' + ', '-': " − ", 'x': ' x ', '/': ' / ', '.': '.', 'c': 'C', 'C': 'C',
-//     0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9'
-// };
-// keycode refs:       =   *   +   -   .   /   0   1   2   3   4   5   6   7   8   9   C   c    x   \
-// var keysToAllow = [13, 42, 43, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 67, 99, 120, 92];
-// console.log(keys.indexOf(event.which));
-// if(keys.indexOf(event.which) !== -1)
-//     parseKeypress(event.which);
 function validateKeypress(){
     var keysAllowed = {
         'Enter': '=', '*': 'x', '-': "−", 'c': 'C', 'C': 'C','x': 'x', '+': '+', '/': '/', '.': '.',
@@ -43,47 +39,46 @@ function buttonClicked(button){
     if (typeof button === "object")
         button = false;
     var currentButton = button || $(this).text();
-    //change this to multiple handlers
     switch(currentButton){
         case "AC":
             clearAll();
-            display("#output","0");
+            updateDisplay("#output","0");
         case "C":
-            clearEntry();
-            display("#input","0");
+            clear();
+            updateDisplay("#input","0");
             break;
         case "←":
             backspace();
-            display("#input","input");
+            updateDisplay("#input","input");
             break;
         case "=":
             calculate(currentButton);
-            display("#output","output");
-            display("#input","input");
+            updateDisplay("#output","output");
+            updateDisplay("#input","input");
             break;
         case "±":
-            plusMinus();
-            display("#input","input");
+            togglePlusMinus();
+            updateDisplay("#input","input");
             break;
         case ".":
             inputDecimal(currentButton);
-            display("#input","input");
+            updateDisplay("#input","input");
             break;
         case "+":
         case "−":
         case "x":
         case "/":
             inputOperator(" " + currentButton + " ");
-            display("#output","output");
+            updateDisplay("#output","output");
             break;
         default:
             inputNumber(currentButton);
-            display("#input","input");
+            updateDisplay("#input","input");
             break;
     }
 }
-function display(target, text){
-    var show = $(target);
+function updateDisplay(target, text){
+    var display = $(target);
     switch(text){
         case "input":
             var string = currentInput.join("");
@@ -98,225 +93,101 @@ function display(target, text){
     if(!string){
         string = 0;
     }
-    show.text(string);
+    display.text(string);
 }
-function clearAll(){
-    savedInputs = [];
-    log(savedInputs, "memory cleared");
-}
-function clearEntry(){
-    currentInput = [];
-    log(currentInput, "inputs cleared");
-}
-function log(array, string){
-    if(!string) string = "current";
-    if(!Array.isArray(array))
-        console.log(array);
-    console.log(array, lastInput(), string);
-}
-function backspace(){
-    console.log("backspace pressed",currentInput);
-    if(currentInput.length > 0) {
-        currentInput.pop();
-    }
-    if(currentInput.length === 1 && currentInput[0] === "0" || currentInput[0] === "-") {
-        currentInput.pop();
-    }
-    log(currentInput);
-}
-function calculate(equalSign, potentialPartialOperand){
-    console.log("= pressed =======================");
-    if (currentInput.length === 0) {
-        console.log("current input empty! special cases!");
-//        if (!isNaOperator(lastButton)) { //1+=
-        if (savedInputs.length % 2 === 0){
-            console.log("last button was operator.  partial operand");
-            var partialOperand = savedInputs.pop();
-            currentInput[currentInput.length] = savedInputs.pop();
-            log(savedInputs, "took out last operator...");
-            log(currentInput, "...added to current input");
-            console.log("lets rewind time a bit.");
-            calculate(equalSign, partialOperand);
-            console.log("operation rollover complete");
-            return;
-        }
-//            savedInputs[2] = savedInputs[0];
-//            numbersToCalculate = savedInputs.slice(0);
-//            savedInputs = [];
-//            savedInputs[0] = do_math(numbersToCalculate);
-//            lastButton = equalSign;
-//            log(savedInputs, "partial operand");
-//            return;
-//        }
-        if (typeof savedInputs[0] === "number"){ //1+1==
-            console.log("repeating; previous calculation was found.");
-            console.log("last operation was " + numbersToCalculate.join(""));
-            numbersToCalculate[0] = savedInputs[0];
-            console.log("but we doing " + numbersToCalculate.join(""));
-            savedInputs = [];
-            savedInputs[0] = do_math(numbersToCalculate);
-            log(savedInputs, "Repeated our savedinputs");
-            return;
-        }
-        console.log("no previous calculation. ending function.");
-        return; //prevents input if nothing is present
-    }
-    if (savedInputs.length === 0 && !potentialPartialOperand) { // 1=
-        console.log("length is 1, not a number.  first input?");
-        savedInputs[0] = currentInput.slice(0).join("");
-        currentInput = [];
-        lastButton = equalSign;
-        log(currentInput, "current");
-        log(savedInputs, "saved");
-        console.log("added to array as string to prevent future repeat");
-        return;
-    }
-    addToInputArray(equalSign);
-    concatInputTil(equalSign);
-    log(currentInput);
-    log(savedInputs, "savedInputs");
 
-    while(savedInputsHaveMD() != undefined){ //does */ first
-        var operatorIsMD = savedInputsHaveMD();
-        var indexOfMD = savedInputs.indexOf(operatorIsMD);
-        if (operatorIsMD = " / "){
-            if (savedInputs[indexOfMD+1] == 0) {
-                savedInputs = ["Cannot divide by Zero"];
-                return;
-            }
-        }
-        numbersToCalculate = savedInputs.splice(indexOfMD-1,3);
-        var tempMD = do_math(numbersToCalculate);
-        savedInputs.splice(indexOfMD-1,0,tempMD);
-        log(savedInputs,"savedInputs")
-    }
-    log(savedInputs, "MD complete, now AS");
-    while(savedInputs.length > 3){ //does +- next
-        var operatorIsAS = savedInputs.find(firstASindex);
-        numbersToCalculate = savedInputs.splice(operatorIsAS-1,3);
-        var answer = do_math(numbersToCalculate);
-        savedInputs.splice(operatorIsAS-1,0,answer);
-        log(savedInputs,"savedInputs")
-    }
-    log(savedInputs, "all math complete");
-    if (savedInputs[1] === "="){
-        log(savedInputs, "equals sign is second item.  we're done?");
-        lastButton = savedInputs.pop();
-        if (potentialPartialOperand){
-            console.log("not done, operation rollover");
-            savedInputs.push(potentialPartialOperand);
-            //added below
-            savedInputs[2] = savedInputs[0];
-            numbersToCalculate = savedInputs.slice(0);
-            savedInputs = [];
-            savedInputs[0] = do_math(numbersToCalculate);
-            // return;
-            //added above
-//            calculate(equalSign);
-        }
-        savedInputs[0] = parseFloat(savedInputs[0]);
-        log(currentInput);
-        log(savedInputs, "saved inputs");
-    }
-}
-function inputDecimal(decimal) {
-    if (currentInput.indexOf(".") > -1) // prevents decimals being added if it already exists
-        return;
-    if (currentInput[0] === undefined) {
-        addToInputArray("0");
-    }
-    addToInputArray(decimal);
-    log(currentInput);
-}
+/** *********
+ ** ** Computations Logic
+ ** ********* **/
 function inputNumber(number){
     if (lastButton === "=") {
-        console.log("last button was =, so preventing concatenation by wiping saved array");
         savedInputs = [];
     }
-    if (currentInput[0] === "0" && currentInput.length === 1 && number === "0") //prevents leading zeroes
+    if (currentInput[0] === "0" && currentInput.length === 1 && number === "0") // Prevents leading zeroes
         return;
-    if (currentInput[0] === "-" && currentInput.length === 2)
+    if (currentInput[0] === "0" && currentInput.length === 1 ||
+        currentInput[0] === "-" && currentInput.length === 2 )
         backspace();
     addToInputArray(number);
     lastButton = number;
-    log(currentInput);
 }
 function inputOperator(operator) {
     if (savedInputs[0] === "Cannot divide by Zero"){
         savedInputs = [];
     }
-    console.log(operator + " clicked           " + operator + "       " + operator);
     if (currentInput.length === 0){
-        log(currentInput, "no current inputs");
-        if (!isNaOperator(lastButton)) { //1+-
-            console.log("...but second operator pressed. overriding " + lastButton);
+        if (isAnOperator(lastButton)) { // Changing Operation Keys aka 1+-*2
             savedInputs[savedInputs.length - 1] = operator;
-            log(savedInputs, "saved inputs");
         }
-        if (lastButton === "=") { // 1+1=+
-            console.log("...equal sign present. partial operand or rollover?");
+        if (lastButton === "=") { // Allows for Operation Rollover/Partial Operand
             lastButton = operator;
             savedInputs[savedInputs.length] = operator;
-            log(savedInputs, "saved inputs");
-            // return;
         }
-        // if (savedInputs.length === 1){ //deprecated?
-        //     if (typeof savedInputs[0] === "string") {
-        //         console.log("..but we have a saved input? lets use that. as a string.");
-        //         currentInput[0] = savedInputs.pop();
-        //         inputOperator(operator);
-        //         return;
-        //     } else {
-        //         log(currentInput, "potential partial operand. readding back operator");
-        //         savedInputs.push(operator);
-        //         lastButton = operator;
-        //         log(savedInputs, "operator added");
-        //         return;
-        //     }
-        // }
-        console.log("we good");
         return;
     }
-    if (isNaOperator(lastButton)) { //if last button is NaOP, could be =, add op, concat til op
-        console.log("first operator pressed");
-        var decimal = currentInput.indexOf(".");
-        if (decimal > -1) {
-            while (currentInput[currentInput.length - 1] === "0") {
-                currentInput.pop();
-            }
-            if (currentInput[currentInput.length-1] === ".") {
-                currentInput.pop();
+    checkDecimals();
+    addToInputArray(operator);
+    concatInputTil(operator);
+    lastButton = operator;
+}
+function calculate(equalSign, potentialPartialOperand){
+    checkDecimals();
+    if (currentInput.length === 0) {
+        if (savedInputs.length % 2 === 0){ // Partial Operand aka 3*=
+            var partialOperand = savedInputs.pop();
+            currentInput[currentInput.length] = savedInputs.pop();
+            calculate(equalSign, partialOperand);
+            return;
+        }
+        if (typeof savedInputs[0] === "number"){ //Operation Repeat aka 1+1==
+            numbersToCalculate[0] = savedInputs[0];
+            savedInputs = [];
+            savedInputs[0] = do_math(numbersToCalculate);
+            return;
+        }
+        return; // Missing Operands aka ====
+    }
+    if (savedInputs.length === 0 && !potentialPartialOperand) { // Missing Operation aka 1=
+        savedInputs[0] = currentInput.slice(0).join("");
+        currentInput = [];
+        lastButton = equalSign;
+        return;
+    }
+    addToInputArray(equalSign);
+    concatInputTil(equalSign);
+    /** *********
+     ** ** ORDER OF OPERATIONS BEGINS HERE
+     ** ********* **/
+    while(savedInputsHaveOperators() != undefined){ // Order: /*+-
+        var operatorPresent = savedInputsHaveOperators();
+        var indexOfOperator = savedInputs.indexOf(operatorPresent);
+        if (operatorPresent = " / "){ // Prevents Division by Zero
+            if (savedInputs[indexOfOperator+1] == 0) {
+                savedInputs = ["Cannot divide by Zero"];
+                return;
             }
         }
-        addToInputArray(operator);
-        concatInputTil(operator);
-        lastButton = operator;
-        log(currentInput);
-        log(savedInputs, "savedInputs");
+        numbersToCalculate = savedInputs.splice(indexOfOperator-1,3);
+        var answer = do_math(numbersToCalculate);
+        savedInputs.splice(indexOfOperator-1,0,answer);
     }
-}
-function addToInputArray(keyPressed){
-    currentInput.push("");
-    currentInput[currentInput.length-1] += keyPressed;
-    return currentInput[currentInput.length-1];
-}
-function concatInputTil(operator){
-    var tempNumbersAsString = currentInput.slice(0,-1).join("");
-    currentInput = [];
-    savedInputs.push(tempNumbersAsString,operator);
-}
-function do_math(num1, op, num2){
-    if(Array.isArray(num1)){
-        console.log('array passed in');
-        var number1 = parseFloat(num1[0]);
-        var number2 = parseFloat(num1[2]);
-        var operator = num1[1];
-    } else {
-        console.log('num1,op,num2 passed in');
-        operator = op;
-        number1 = parseFloat(num1);
-        number2 = parseFloat(num2);
+    /** *********
+     ** ** ORDER OF OPERATIONS ENDS HERE
+     ** ********* **/
+    lastButton = savedInputs.pop();
+    if (potentialPartialOperand){ //Operation Rollover or Partial Operand aka 3+3+=+=
+        savedInputs.push(potentialPartialOperand);
+        savedInputs[2] = savedInputs[0];
+        numbersToCalculate = savedInputs.slice(0);
+        savedInputs = [];
+        savedInputs[0] = do_math(numbersToCalculate);
     }
+    savedInputs[0] = parseFloat(savedInputs[0]);
+}
+function do_math(array){
+    var number1 = parseFloat(array[0]);
+    var number2 = parseFloat(array[2]);
+    var operator = array[1];
     switch(operator){
         case ' + ':
             return number1 + number2;
@@ -326,33 +197,38 @@ function do_math(num1, op, num2){
             return number1 / number2;
         case ' x ':
             return number1 * number2;
-        case ' = ':
-            console.log("wtf you messed up");
-            return "= in pos2?";
         default:
-            console.log("error: num1 = " + num1);
-            return "defaulted. bug";
+            return "Calculator Error";
     }
 }
-function isNaOperator(lastButton){
-    switch(lastButton){
-        case " + ":
-        case " − ":
-        case " x ":
-        case " / ":
-            return false;
-        default:
-            return true;
+
+/** *********
+ ** ** Button Inputs
+ ** ********* **/
+function inputDecimal(decimal) {
+    if (currentInput.indexOf(".") > -1) // Multiple Decimals aka 1...1
+        return;
+    if (currentInput[0] === undefined) {
+        addToInputArray("0");
+    }
+    addToInputArray(decimal);
+}
+function backspace(){
+    if(currentInput.length > 0) {
+        currentInput.pop();
+    }
+    if(currentInput.length === 1 && currentInput[0] === "0" || // Prevents Leading Zero if deleting decimal
+        currentInput.length === 1 && currentInput[0] === "-"){ // Prevents [+-] > [.] -> [Bckspc]x2 issues
+        currentInput.pop();
     }
 }
-function changeSign(pos){
-    return pos = !pos;
+function clearAll(){
+    savedInputs = [];
 }
-function plusMinus(){
-    positive = changeSign(positive);
-    toggleNegative();
+function clear(){
+    currentInput = [];
 }
-function toggleNegative(){
+function togglePlusMinus(){
     if (currentInput[0] !== "-"){
         if (currentInput[0] === undefined) {
             addToInputArray("0");
@@ -361,22 +237,35 @@ function toggleNegative(){
     } else {
         currentInput.shift();
     }
-    log(currentInput);
 }
-function savedInputsHaveMD(){
-    return savedInputs.find(firstMD);
+
+/** *********
+ ** ** Repeated Functions
+ ** ********* **/
+function checkDecimals(){
+    var decimal = currentInput.indexOf(".");
+    if (decimal > -1) {
+        while (currentInput[currentInput.length - 1] === "0") { // Prevents trailing zeroes
+            currentInput.pop();
+        }
+        if (currentInput[currentInput.length-1] === ".") { // Prevents unnecessary decimals
+            currentInput.pop();
+        }
+    }
 }
-function firstMD(inArray){
-    return [" / "," x "].indexOf(inArray) > -1;
+function addToInputArray(keyPressed){
+    currentInput.push("");
+    currentInput[currentInput.length-1] += keyPressed;
+    return currentInput[currentInput.length-1];
 }
-function firstASindex(inArray){
-    return [" + "," − "].indexOf(inArray) > -1;
+function concatInputTil(operator){
+    var numbersAsOneString = currentInput.slice(0,-1).join("");
+    currentInput = [];
+    savedInputs.push(numbersAsOneString,operator);
 }
-function lastOperatorIn(array){ //.find(lastOperatorIn(array), returns +-*/
-    return [" + "," − "," x "," / "].indexOf(array) > -1;
+function isAnOperator(input){ //.find(isAnOperator(array) returns /*-+ as string.
+    return [" / "," x "," + "," − "].indexOf(input) > -1;
 }
-function lastInput (){
-    return currentInput[currentInput.length-1] === undefined ?
-        savedInputs[savedInputs.length-1] :
-        currentInput[currentInput.length-1];
+function savedInputsHaveOperators(){
+    return savedInputs.find(isAnOperator);
 }
